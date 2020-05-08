9ac9193b7f6c0d8be55689310809a3187fc900fc
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-05-07 19:21:26 -0700

    added bootstrap grid to component style

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
       <Button theme={"midnight"} value={"Submit"} size={"monster"} />
 
-      <Form theme={"sunset"} placeholder1={"Your Name"} placeholder2={"Your Phone Number (optional)"} placeholder3={"Email"} btnValue1={'Submit'} btnValue2={'Clear'} />
+      <Form theme={"midnight"} placeholder1={"Your Name"} placeholder2={"Your Phone Number (optional)"} placeholder3={"Email"} btnValue1={'Submit'} btnValue2={'Clear'} />
 
     </div>
   );
