e0f7d4f5ebcb2a7571c339b7ca9a9d5297445e0b
Marie Lenac <lenacm02@gmail.com>
2020-05-09 09:42:57 -0700

    version-bump

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Card from "./Card";
+import Figure from "./Figure";
+import Footer from "./Footer";
+import Form from "./Form";
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Social from "./Social";
+
+export { Button, Card, Figure, Footer, Form, Navbar, Search, Social };
